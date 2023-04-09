13ad293912bbee10a796868ff3dd0209e065ec44
Michy#4320 <96814843+MichyMedia@users.noreply.github.com>
2023-04-09 15:36:48 +1000

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,374 @@
+
+	['bigmac'] = {
+		label = 'BigMac Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['bigmac_meal'] = {
+		label = 'BigMac Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['mcchicken'] = {
+		label = 'McChicken Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['mcchicken_meal'] = {
+		label = 'McChicken Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['quarterpounder'] = {
+		label = 'Quarter Pounder Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['quarterpounder_meal'] = {
+		label = 'Quarter Pounder Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['dquarterpounder'] = {
+		label = 'Double Quarter Pounder Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['dquarterpounder_meal'] = {
+		label = 'Double Quarter Pounder Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['filet-o-fish'] = {
+		label = 'Filet-O-Fish Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['filet-o-fish_meal'] = {
+		label = 'Filet-O-Fish Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['cheeseburger'] = {
+		label = 'Cheeseburger Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['cheeseburger_meal'] = {
+		label = 'Cheeseburger Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['dcheeseburger'] = {
+		label = 'Double Cheeseburger Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['dcheeseburger_meal'] = {
+		label = 'Double Cheeseburger Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['tcheeseburger'] = {
+		label = 'Tripple Cheeseburger Burger',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['tcheeseburger_meal'] = {
+		label = 'Trpple Cheeseburger Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'burger',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['6pnuggets'] = {
+		label = '6 Pack Chicken Nuggets',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['6pnuggets_meal'] = {
+		label = '6 Pack Chicken Nugget Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['10pnuggets'] = {
+		label = '10 Pack Chicken Nuggets',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['10pnuggets_meal'] = {
+		label = '10 Pack Chicken Nugget Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['20pnuggets'] = {
+		label = '20 Pack Chicken Nuggets',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['20pnuggets_meal'] = {
+		label = '20 Pack Chicken Nugget Meal',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 200000 },
+			anim = 'eating',
+			prop = 'hsp',
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['sfries'] = {
+		label = 'Small Fries',
+		degrade = 160,--2160 = 36hours
+		weight = 180,
+		client = {
+			status = { hunger = 60000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['mfries'] = {
+		label = 'Meduim Fries',
+		degrade = 160,--2160 = 36hours
+		weight = 200,
+		client = {
+			status = { hunger = 80000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['lfries'] = {
+		label = 'Large Fries',
+		degrade = 160,--2160 = 36hours
+		weight = 220,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['hotapplepie'] = {
+		label = 'Hot Apple Pie',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['ssconef'] = {
+		label = 'Soft Serve Cone With A Flake',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['sscone'] = {
+		label = 'Soft Serve Cone',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+	
+	['mflurryo'] = {
+		label = 'McFlurry with Oreos',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
+
+	['mflurrym'] = {
+		label = 'McFlurry with M&Ms',
+		degrade = 160,--2160 = 36hours
+		weight = 110,
+		client = {
+			status = { hunger = 100000 },
+			usetime = 2500,
+			----notification = 'You ate a delicious burger'
+		},
+	},
